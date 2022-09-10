64b64ce1055b69a68144a9434db479e29667ec75
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-09-11 01:03:30 +0430

    feat(`m-odal-filter`): modal filter component

---
+++ src/components/m-odal-filter.ts
@@ -0,0 +1,146 @@
+import {SignalInterface} from '@alwatr/signal';
+import {Task} from '@lit-labs/task';
+import {css, html, nothing} from 'lit';
+import {customElement} from 'lit/decorators/custom-element.js';
+import {state} from 'lit/decorators/state.js';
+
+import {AppElement} from '../app-debt/app-element';
+
+import type {locale} from '../config';
+import type {CategoryInterface} from '../types/category';
+import type {ProductFilter} from '../types/product-filter';
+import type {RadioGroupCustomEvent} from '@ionic/core';
+import type {TemplateResult, CSSResult, PropertyValues} from 'lit';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'm-odal-filter': MOdalFilter;
+  }
+}
+
+@customElement('m-odal-filter')
+export class MOdalFilter extends AppElement {
+  static override styles = [
+    ...(<CSSResult[]>AppElement.styles),
+
+    css`
+      :host {
+        display: block;
+      }
+      ion-footer ion-button {
+        margin: 0 !important;
+
+        --padding-start: 12vw;
+        --padding-end: 12vw;
+      }
+    `,
+  ];
+
+  @state() protected _categoryList: Record<string, CategoryInterface> = {};
+  @state() protected _filters: ProductFilter = {category: 'all'};
+
+  protected _modalPageSignal = new SignalInterface('modal-page');
+  protected _categoryListSignal = new SignalInterface('category-list');
+  protected _productListFilterSignal = new SignalInterface('product-list-filter');
+  protected _dataTask = new Task(this, async (): Promise<Record<string, CategoryInterface>> => {
+    const data = await this._categoryListSignal.request(false);
+
+    this._categoryList = data;
+    this._logger.logProperty('_categoryList', {data});
+
+    return data;
+  });
+
+  override render(): TemplateResult {
+    return html`
+      <ion-header>
+        <ion-toolbar>
+          <ion-buttons slot="end">
+            <ion-button @click=${this._closeModal}>
+              <ion-icon slot="icon-only" name="close"></ion-icon>
+            </ion-button>
+          </ion-buttons>
+          <ion-title>Filter</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content fullscreen>
+        ${this._dataTask.render({
+    pending: () => html``,
+    complete: () => html`<ion-list> ${this._renderCategoryFilters()} </ion-list>`,
+  })}
+      </ion-content>
+      <ion-footer>
+        <ion-button expand="full" size="large" ?disabled=${!this._canApplyFilters} @click=${this._applyFilters}>
+          <ion-icon slot="start" name="funnel-outline"></ion-icon>
+          <ion-label>Apply filter</ion-label>
+        </ion-button>
+      </ion-footer>
+    `;
+  }
+  override async firstUpdated(_changedProperties: PropertyValues): Promise<void> {
+    super.firstUpdated(_changedProperties);
+    this._dataTask.run();
+
+    if (!this._productListFilterSignal.value?.filter) {
+      await this._productListFilterSignal.request({category: 'all'});
+    } else if (this._productListFilterSignal.value?.filter) {
+      this._filters = this._productListFilterSignal.value.filter;
+    }
+  }
+
+  protected _renderCategoryFilters(): TemplateResult | typeof nothing {
+    if (!this._categoryListSignal.value) return nothing;
+
+    const categoryListTemplate = Object.values(this._categoryListSignal.value).map(
+        (category) => html`
+        <ion-item>
+          <ion-radio slot="start" value=${category.slug} name="category"></ion-radio>
+          <ion-label>${category.title[<locale['code']> this._localize.lang()]}</ion-label>
+        </ion-item>
+      `,
+    );
+    return html`
+      <ion-radio-group
+        value=${this._productListFilterSignal.value?.filter.category ?? 'all'}
+        name="category"
+        @ionChange=${this._filterCategoryChange}
+      >
+        <ion-list-header>
+          <ion-label> By Categories: </ion-label>
+        </ion-list-header>
+
+        <ion-item>
+          <ion-radio slot="start" value="all"></ion-radio>
+          <ion-label>All</ion-label>
+        </ion-item>
+
+        ${categoryListTemplate}
+      </ion-radio-group>
+    `;
+  }
+
+  protected _closeModal(): void {
+    this._modalPageSignal.value?.dismiss();
+  }
+  protected _filterCategoryChange(event: RadioGroupCustomEvent): void {
+    const category = <ProductFilter['category']>event.detail.value;
+    this._logger.logMethodArgs('_filterCategoryChange', {category: category, event});
+    this._filters = {...this._filters, category: category};
+  }
+  protected async _applyFilters(): Promise<void> {
+    await this._productListFilterSignal.request(this._filters);
+    this._closeModal();
+  }
+
+  protected get _canApplyFilters(): boolean {
+    if (!this._productListFilterSignal.value) return false;
+
+    if (this._filters.category !== this._productListFilterSignal.value.filter.category) {
+      this._logger.logProperty('_canApplyFilters', true);
+      return true;
+    }
+
+    this._logger.logProperty('_canApplyFilters', false);
+    return false;
+  }
+}
