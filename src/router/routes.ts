bf82c35270bcfeb151b153e59a7519ec229f3eb0
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-09-24 05:15:46 +0330

    refactor(`router`): define types

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,6 @@
 import {html} from 'lit';
 
-import {navigation} from '../config';
+import type {Route} from '../types/route';
 
 import '../pages/page-home';
 import '../pages/page-product-list';
@@ -8,8 +8,8 @@ import '../pages/page-product-detail';
 import '../pages/page-cart';
 import '../pages/page-about';
 
-const routes: navigation = {
-  products: {
+const routes: Record<string, Route> = {
+  'products': {
     title: 'products',
     icon: 'grid',
     render: (route) => {
@@ -19,24 +19,29 @@ const routes: navigation = {
       return html`<page-product-list class="ion-page" type="card"></page-product-list>`;
     },
   },
-  cart: {
+  'cart': {
     title: 'cart',
     icon: 'cart',
     render: () => html`<page-cart class="ion-page"></page-cart>`,
   },
-  home: {
+  'home': {
     title: 'home',
     icon: 'home',
     render: () => html`<page-home class="ion-page"></page-home>`,
   },
-  account: {
+  'account': {
     title: 'account',
     icon: 'person',
     render: () => html`<page-home class="ion-page"></page-home>`,
   },
-  contact: {
+  'contact': {
     title: 'contact_us',
     icon: 'call',
+    render: () => html`<page-about class="ion-page"></page-about>`,
+  },
+  '404': {
+    title: '404',
+    show_in_bar: false,
     render: () => html`<page-about class="ion-page"></page-about>`,
   },
 };
