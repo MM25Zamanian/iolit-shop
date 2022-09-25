c7a7206552b178cbd5c3695630adad44003174f7
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-09-25 07:59:34 +0330

    docs: add jsdoc

--- src/utilities/load-data-cache.ts
+++ src/utilities/load-data-cache.ts
@@ -4,6 +4,14 @@ import type {FetchOptions} from '@alwatr/fetch';
 
 const fetchCache = new Map<string, unknown>();
 
+/**
+ * If the URL is in the cache, return the cached data, otherwise fetch the data and cache it.
+ * @param {string} url - The URL to fetch from.
+ * @param [queryParameters] - This is an object that contains the query parameters you want to pass to
+ * the URL.
+ * @param {FetchOptions} [options] - This is an object that can contain the following properties:
+ * @returns A promise that resolves to a ResponseType
+ */
 export async function loadDataCaching<ResponseType extends Record<string | number, unknown>>(
     url: string,
     queryParameters?: Record<string | number, string | number | boolean>,
--- src/utilities/register-sw.ts
+++ src/utilities/register-sw.ts
@@ -1,6 +1,11 @@
 import {createLogger} from '@alwatr/logger';
 import {SignalInterface} from '@alwatr/signal';
 
+/**
+ * It registers a service worker, and if it's successful, it adds a listener to the service worker's
+ * update event, and it adds a listener to the signal interface's update event
+ * @returns A promise that resolves when the service worker is registered.
+ */
 export default async function registerSW(): Promise<void> {
   const logger = createLogger('register-sw');
   const signal = new SignalInterface('sw-update');
