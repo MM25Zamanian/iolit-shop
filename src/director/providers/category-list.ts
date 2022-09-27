8f7d9f2d3f64f40ba484e7c6fd9b3c770c9f7610
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-09-28 00:49:04 +0330

    refactor(`director`): providers & loader & signals

---
+++ src/director/providers/category-list.ts
@@ -0,0 +1,12 @@
+import {categoryList} from '../load-data';
+import {categoryListSignal} from '../signal';
+
+import type {CategoryInterface} from '../../types/category';
+
+export default async function categoryListProvider(): Promise<Record<string, CategoryInterface>> {
+  return await categoryList();
+}
+
+categoryListSignal.setProvider(async function(): Promise<Record<string, CategoryInterface>> {
+  return await categoryList();
+});
