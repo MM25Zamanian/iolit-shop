002139c44d6f148be9be83aa0c425aa8b2f13fad
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-09-30 08:54:49 +0330

    feat(`cart`): indexed db

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,5 +1,10 @@
 import {ProductInterface} from './product';
 
+export interface CartMinimizeInterface {
+  productId: string;
+  quantity: number;
+}
+
 export interface CartInterface {
   product: ProductInterface;
   quantity: number;
